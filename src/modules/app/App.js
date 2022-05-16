9e764ad348090286f80aa66b494faba28a548ad8
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-16 10:06:26 +0300

    Progress

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -1,9 +1,30 @@
-
+import { useState, useEffect } from "react";
 
 function App() {
+
+const [isFetching, setIsFetching] = useState(true)
+
+useEffect(() => {
+fetch('/api/')
+.then(res => res.json)
+.then(res => { setIsFetching(false)
+console.log(res)
+})
+}, [])
+
   return (
     <div className="App">
-Test
+    {isFetching ? (<div className='loader'>Loading...</div>)
+    :
+(
+<ul>
+  <li>Chore to do</li>
+  <li>Chore to do</li>
+  <li>Chore to do</li>
+  <li>Chore to do</li>
+  <li>Chore to do</li>
+</ul>
+    )}
     </div>
   );
 }
